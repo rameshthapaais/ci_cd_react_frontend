a4666d3440fba0349804ce12cf37132ae32f0b27
rameshthapaais <rath842@ess.ais.ac.nz>
2024-07-19 02:32:44 +1200

    Added changes in react-fronntend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Nabbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 import AddStudent from './components/Student';
 import AddMarks from './components/StudentMarks';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
-    <Nabbar/>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/student" element={<AddStudent />} />
-        <Route path="/student_marks" element={<AddMarks />} />
-    </Routes>
+      <Nabbar />
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/student" element={<AddStudent />} />
+          <Route path="/student_marks" element={<AddMarks />} />
+        </Routes>
+
+      
+      <br />
     </div>
   );
 }
